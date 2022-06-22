4e3cc2aba5ceeb496b1444aae57db3fe11ead76b
Alain Hélaïli <helaili@github.com>
2022-06-22 12:31:49 +0000

    Cleaned up the code

--- api/db.js
+++ api/db.js
@@ -1,10 +1,5 @@
 const app = require('express')()
-const { MongoClient, ObjectId } = require('mongodb')
-
-app.all('/testDocument', (_, res) => {
-  const doc = { title: 'space oddity' }
-  res.json({ data: doc })
-})
+const { MongoClient } = require('mongodb')
 
 // Get all songs
 app.all('/songs', async (_, res) => {
@@ -13,16 +8,6 @@ app.all('/songs', async (_, res) => {
 
   client.db('mydb').collection('songs').find().toArray().then((songs) => {
     res.json({ data: songs })
-  })
-})
-
-// Get a song by id
-app.all('/song/:id', async (req, res) => {
-  const client = new MongoClient('mongodb://localhost')
-  await client.connect()
-
-  client.db('mydb').collection('songs').findOne({ _id: ObjectId(req.params.id) }).then((song) => {
-    res.json({ data: song })
   })
 })
 
